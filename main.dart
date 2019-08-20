0787af7b6551b346d62d22c1cd6cf15cecf7faea
nikunjmalani <malaniniks@gmail.com>
2019-08-20 23:07:38 +0530

    string interpolation

--- main.dart
+++ main.dart
@@ -1,8 +1,10 @@
 void main() {
   final x = 10;
   const y = 20;
+  var myMessage = 'hello';
+  var myMessage2 = 'dart';
 
   // x = 30; //genrate error final can not change value ones declare...
 
-  print('$x and $y');
+  print('$myMessage  $myMessage2');
 }
